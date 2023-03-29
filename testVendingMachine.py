10678d4900d9495f5058a7c6d71d3ae12f4c9452
Benedikt Chmielewski <bc222dz@student.lnu.se>
2023-03-30 00:29:41 +0200

    Create test case for empty Vending Machine

--- testVendingMachine.py
+++ testVendingMachine.py
@@ -1,10 +1,17 @@
 import unittest
+import VendingMachine
 
 
 class testVendingMachine(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
 
-    def test_sum(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+    # def test_sum(self):
+        # self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+    
+    def test_empty_stock_is_zero(self):
+        empty_vm = VendingMachine.VendingMachine()
+        self.assertEqual(empty_vm.get_stock(), 0, "Should be 0")
 
 if __name__ == '__main__':
     unittest.main()
