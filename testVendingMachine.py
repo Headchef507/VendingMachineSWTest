2483754686b44123d7e293e30b79dee1b543af7e
HeadChef507 <arnarlarsen@gmail.com>
2023-03-29 21:38:24 +0000

    Update testVendingMachine.py
    create test case test_request_bottle_where_coinIsInserted

--- testVendingMachine.py
+++ testVendingMachine.py
@@ -10,6 +10,9 @@ class testVendingMachine(unittest.TestCase):
     def test_request_bottle(self):
         vendingMachine = VendingMachine.VendingMachine()
         self.assertEqual(1,vendingMachine.request_bottle())
+    
+    def test_request_bottle_where_coinIsInserted(self):
+        self.assertEqual(1,1)
 
 if __name__ == '__main__':
     unittest.main()
