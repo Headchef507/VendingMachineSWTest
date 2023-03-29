a98d9ab63ea392b598145195214bd35ee550096b
HeadChef507 <arnarlarsen@gmail.com>
2023-03-29 21:12:33 +0000

    testVendingMachine
    create test case

--- testVendingMachine.py
+++ testVendingMachine.py
@@ -5,6 +5,9 @@ class testVendingMachine(unittest.TestCase):
 
     def test_sum(self):
         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+    
+    def test_request_bottle(self):
+        self.assertEqual(1,0)
 
 if __name__ == '__main__':
     unittest.main()
