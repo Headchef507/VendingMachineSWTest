aee42cb6cc81bf4a412ca6c6c09f4c2828b1e2bd
Benedikt Chmielewski <bc222dz@student.lnu.se>
2023-03-30 00:55:29 +0200

    Add TC to test for other value

--- testVendingMachine.py
+++ testVendingMachine.py
@@ -12,6 +12,10 @@ class testVendingMachine(unittest.TestCase):
     def test_empty_stock_is_zero(self):
         empty_vm = VendingMachine.VendingMachine()
         self.assertEqual(empty_vm.get_stock(), 0, "Should be 0")
+    
+    def test_5_stock_is_5(self):
+        five_vm = VendingMachine.VendingMachine(5)
+        self.assertEqual(five_vm.get_stock(), 5, "Should be 5")
 
 if __name__ == '__main__':
     unittest.main()
