3aae35d226c9c5a34168f0b12746b03c0aed138e
HeadChef507 <arnarlarsen@gmail.com>
2023-04-01 20:48:02 +0000

    Update VendingMachine.py
    test_insertCoin_stockIsEmpty_exception now works but test_request_bottle_where_coinIsInserted now fails

--- VendingMachine.py
+++ VendingMachine.py
@@ -1,12 +1,16 @@
 class VendingMachine:
     coininsert = False
+    stock = 0
     def request_bottle(self):
         if(self.coininsert):
             return 1
         else:
             return 0
     def insert_coin(self):
-        self.coininsert = True
+        if(self.stock > 0):
+            self.coininsert = True
+        else:
+           raise Exception("Stock is empty, please refill to continue") 
     def __init__(self):
         #self.inventory = inventory
         print('hello world')
