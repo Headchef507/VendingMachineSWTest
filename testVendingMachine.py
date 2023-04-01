34f7383fa7e4f128178f0ab6b332a7ba8bd799bb
HeadChef507 <arnarlarsen@gmail.com>
2023-04-01 21:25:15 +0000

    Update testVendingMachine.py
    rename to test_request_bottle_where_coinIsNotInserted_exception and change it so it throws an exception instead

--- testVendingMachine.py
+++ testVendingMachine.py
@@ -4,9 +4,10 @@ import VendingMachine
 
 class testVendingMachine(unittest.TestCase):
 
-    def test_request_bottle_where_coinIsNotInserted(self):
+    def test_request_bottle_where_coinIsNotInserted_exception(self):
         vendingMachine = VendingMachine.VendingMachine()
-        self.assertEqual(0,vendingMachine.request_bottle())
+        with self.assertRaises(Exception):
+            vendingMachine.request_bottle()
     
     def test_request_bottle_where_coinIsInserted(self):
         vendingMachine = VendingMachine.VendingMachine(1)
@@ -17,7 +18,7 @@ class testVendingMachine(unittest.TestCase):
         vendingMachine = VendingMachine.VendingMachine()
         with self.assertRaises(Exception):
             vendingMachine.insert_coin()
-            
+
     def test_insertCoin_twice_exception(self):
         vendingMachine = VendingMachine.VendingMachine(1)
         vendingMachine.insert_coin()
