d5f092cbdfe35aa99322218146f00611ed6be01d
Benedikt Chmielewski <bc222dz@student.lnu.se>
2023-04-03 10:57:42 +0200

    Add check for string in init.

--- VendingMachine.py
+++ VendingMachine.py
@@ -2,6 +2,8 @@ class VendingMachine:
     def __init__(self, stock=0):
         if stock < 0:
             raise ValueError("The class VendingMachine must be initialized with a positive value")
+        if type(stock) == str:
+            raise TypeError("The inputted value must be an integer or empty.")
         self.stock = stock
     
     def get_stock(self):
