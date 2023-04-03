241164880e5ef1f965677f4b68eb58e3f5a3d4a5
HeadChef507 <arnarlarsen@gmail.com>
2023-04-03 10:36:15 +0000

    Update testVendingMachine.py
    organize by positive and negative test cases

--- testVendingMachine.py
+++ testVendingMachine.py
@@ -4,22 +4,11 @@ import VendingMachine
 
 class testVendingMachine(unittest.TestCase):
 
-    def test_request_bottle_where_coinIsNotInserted_exception(self):
-        vendingMachine = VendingMachine.VendingMachine()
-        with self.assertRaises(Exception):
-            vendingMachine.request_bottle()
-    
+#Postive TC
     def test_request_bottle_where_coinIsInserted(self):
         vendingMachine = VendingMachine.VendingMachine(1)
         vendingMachine.insert_coin()
         self.assertEqual(1,vendingMachine.request_bottle())
-
-    def test_request_bottle_twice_exception(self):
-        vendingMachine = VendingMachine.VendingMachine(2)
-        vendingMachine.insert_coin()
-        vendingMachine.request_bottle()
-        with self.assertRaises(Exception):
-            vendingMachine.request_bottle()
 
     def test_request_bottle_check_if_stack_decreases_now_stack_is_empty(self):
         vendingMachine = VendingMachine.VendingMachine(1)
@@ -32,6 +21,20 @@ class testVendingMachine(unittest.TestCase):
         vendingMachine.insert_coin()
         vendingMachine.request_bottle()
         self.assertEqual(1,vendingMachine.stock)
+
+#Negative TC
+
+    def test_request_bottle_where_coinIsNotInserted_exception(self):
+        vendingMachine = VendingMachine.VendingMachine()
+        with self.assertRaises(Exception):
+            vendingMachine.request_bottle()
+
+    def test_request_bottle_twice_exception(self):
+        vendingMachine = VendingMachine.VendingMachine(2)
+        vendingMachine.insert_coin()
+        vendingMachine.request_bottle()
+        with self.assertRaises(Exception):
+            vendingMachine.request_bottle()
     
     def test_insertCoin_stockIsEmpty_exception(self):
         vendingMachine = VendingMachine.VendingMachine()
