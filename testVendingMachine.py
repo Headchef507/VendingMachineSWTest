a5ebd2fae2ed43ec0dad34d956a21f07b0d1db4a
HeadChef507 <arnarlarsen@gmail.com>
2023-04-03 10:07:57 +0000

    Update testVendingMachine.py
    create test_request_bottle_check_if_stack_decreases_stack_is_not_empty and rename test_request_bottle_check_if_stack_decreases_now_stack_is_empty

--- testVendingMachine.py
+++ testVendingMachine.py
@@ -21,11 +21,14 @@ class testVendingMachine(unittest.TestCase):
         with self.assertRaises(Exception):
             vendingMachine.request_bottle()
 
-    def test_request_bottle_check_if_stack_decreases(self):
+    def test_request_bottle_check_if_stack_decreases_now_stack_is_empty(self):
         vendingMachine = VendingMachine.VendingMachine(1)
         vendingMachine.insert_coin()
         vendingMachine.request_bottle()
         self.assertEqual(0,vendingMachine.stock)
+
+    def test_request_bottle_check_if_stack_decreases_stack_is_not_empty(self):
+        self.assertEqual(1,0)
     
     def test_insertCoin_stockIsEmpty_exception(self):
         vendingMachine = VendingMachine.VendingMachine()
