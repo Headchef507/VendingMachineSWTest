a1f54db7ca9ac315da21d2fb6f0e82e05b1cf4a7
Benedikt Chmielewski <bc222dz@student.lnu.se>
2023-04-03 10:51:26 +0200

    Raise error, if init-value is negative.

--- VendingMachine.py
+++ VendingMachine.py
@@ -1,5 +1,7 @@
 class VendingMachine:
     def __init__(self, stock=0):
+        if stock < 0:
+            raise ValueError("The class VendingMachine must be initialized with a positive value")
         self.stock = stock
     
     def get_stock(self):
